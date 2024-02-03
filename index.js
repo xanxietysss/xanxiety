91e60c41d8f57c786f237dfb790d5591384e945c
xanxietysss <158645796+xanxietysss@users.noreply.github.com>
2024-02-02 19:44:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,19 +23,20 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('')
+    .setApplicationId('1103491371005915176')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
-    .setState('Recording')
-    .setName('')
-    .setDetails(`Valorant [${formatTime()}]`)
-    .setStartTimestamp(Date.now())
- .setAssetsLargeImage('') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Boring') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Twitch') //Text when you hover the Small image
-    .addButton('Watch', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    .addButton('Donate', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    .setURL('https://www.youtube.com/watch?v=zdfacEpzDIA') //Must be a youtube video link 
+    .setState('confusion')
+    .setName('xanxietys')
+    .setDetails('entry log')
+
+
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/852639023482798161/1190099463784644730/giphy_2.gif?ex=65a0917f&is=658e1c7f&hm=21fa458206b16dd6a0074a0fc494067759a96a805b6d676475c58d84e3b0d713&') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('entry four') //Text when you hover the Large image
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/852639023482798161/1190100720532340866/d309ca96cbbd106dcc3b9549219152d1.gif?ex=65a092aa&is=658e1daa&hm=feadc1be08b2f31efdb41a0a9ab90034be6122870179106b7450eccd1b2bfc26&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('ok') //Text when you hover the Small image
+    .addButton('percs', 'https://www.youtube.com/watch?v=72noANFOhdA')
+    .addButton('xans', 'https://www.youtube.com/watch?v=Gbqa9n1XOes');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
@@ -44,7 +45,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
+      const newDetails = `entry log `;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
