ae2f374aef508d3e66b4cba2032b7901317ffa69
xanxietysss <158645796+xanxietysss@users.noreply.github.com>
2024-05-03 20:41:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ client.on('ready', async () => {
     .setDetails('#losersclub')
 
 
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1226636743642714205/1229454303454957699/IMG_6225.jpg?ex=6635038c&is=6633b20c&hm=77d5287a1a81d4d7a2f4ce916fa1f2a2fcb5505ca117747b492fc1e2fb2c7044&') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1226636743642714205/1229455572529713212/IMG_5697.jpg?ex=6636563a&is=663504ba&hm=3c5d57c27cf67c424d1c1a3c0a015ed3be34f7d413657964e6d803fa718830a1&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('loser') //Text when you hover the Large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/867485720012914689/1235742551563309087/Black.png?ex=66357a6f&is=663428ef&hm=1508ecaa6ac4af102c254a22ce05860549ce43590356ed2c9630fdbb741517eb&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/867485720012914689/1236114758898421833/Black-Background-PNG-Photo.png?ex=6636d514&is=66358394&hm=c38096e23b66a71c08e6319711eef7d8b210b15c0d61857252e4a2cd50bbb147&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('ok') //Text when you hover the Small image
     .addButton('percs', 'https://www.youtube.com/watch?v=72noANFOhdA')
     .addButton('xans', 'https://www.youtube.com/watch?v=Gbqa9n1XOes');
