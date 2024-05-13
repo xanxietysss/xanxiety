6f93385282bc7094c796a385eea56f56d9c8248f
xanxietysss <158645796+xanxietysss@users.noreply.github.com>
2024-05-12 21:26:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on('ready', async () => {
     .setDetails('#losersclub')
 
 
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/867485720012914689/1239359789775982602/IMG_8846.png?ex=6642a340&is=664151c0&hm=8d4053dec86353398cf4bfaed0e265d21c876297fc1818345c49cd64c1355d3b&') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/867485720012914689/1239387984940372108/IMG_8551.jpg?ex=6642bd82&is=66416c02&hm=4ca276bed64e21390e837c4f2c46e309714565469218ba4c2f9c57b8528e682e&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('loser') //Text when you hover the Large image
     .setAssetsSmallImage('https://cdn.discordapp.com/attachments/867485720012914689/1239359790258323456/BA08AE7C-412E-42EC-992B-28BAB55CD28D.jpg?ex=6642a340&is=664151c0&hm=f85c3fff22886d165dcb0c3248e48c41d76fff241b69174588a013f7e4262795&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('ok') //Text when you hover the Small image
