a0bb6b692b3dd5376f37410fae3ec04dc49b75ba
xanxietysss <158645796+xanxietysss@users.noreply.github.com>
2024-07-10 03:52:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ client.on('ready', async () => {
     .setApplicationId('1103491371005915176')
     .setType('STREAMING')
     .setURL('https://www.youtube.com/watch?v=zdfacEpzDIA') //Must be a youtube video link 
-    .setState('r,l')
+    .setState('x_-')
     .setName('xanxietys')
     .setDetails('#jugg')
 
 
  .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1252694799379861606/1260502879069016136/750492DD-32E9-448F-9777-D97B6692F681.jpg?ex=668f8e49&is=668e3cc9&hm=3a306a0e303d168a6bc6c755c2714173aaba99b44ec2c91151e70e60fa2a50da&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('loser') //Text when you hover the Large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1226636743642714205/1229485289269886986/Black.png?ex=668a2927&is=6688d7a7&hm=c66414010c3e6eacc86cd8cb3c48309c7b3b4e9afa74fab5272a2b19545c9d2f&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1252694799379861606/1260503714440020039/Solid_black.png?ex=668f8f10&is=668e3d90&hm=a9f441b82d2c088e031f7bf9e38b7a0af844604a8ab9e7d25c4464dd1485488a&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('ok') //Text when you hover the Small image
     .addButton('percs', 'https://www.youtube.com/watch?v=72noANFOhdA')
     .addButton('xans', 'https://www.youtube.com/watch?v=Gbqa9n1XOes');
