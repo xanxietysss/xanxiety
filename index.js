167b58680d264cbc8cb77e9e9cf3c44181d9e954
xanxietysss <158645796+xanxietysss@users.noreply.github.com>
2024-07-28 04:05:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.on('ready', async () => {
 
 
  .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1245540160758747250/1266941857166786735/IMG_5182.jpg?ex=66a6fb0d&is=66a5a98d&hm=4b4b73743bed2d683429090f590bf5145cd3ac53a07c30775bce022e472901a2&') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('really rich') //Text when you hover the Large image
+    .setAssetsLargeText('what we have') //Text when you hover the Large image
 
     .setAssetsSmallText('ok') //Text when you hover the Small image
     .addButton('percs', 'https://www.youtube.com/watch?v=pEWB_PqmNpE')
