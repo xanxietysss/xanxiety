96e8e0a2e9796f63895e1264845f5d3cc2d74520
xanxietysss <158645796+xanxietysss@users.noreply.github.com>
2024-09-08 17:16:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on('ready', async () => {
     .setDetails('@newmargielas')
 
 
- .setAssetsLargeImage('https://cdn.discordapp.com/avatars/761614814370988062/2faf307f4315e3e7de9f0e9e070f7684.png?size=1024') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1252694799379861606/1282449497913299127/2faf307f4315e3e7de9f0e9e070f7684.png?ex=66df65ab&is=66de142b&hm=0b33b2a22953faf2cca96bb2a7c2aa02566fb4c4ff93854a72319f5f559910c5&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('##') //Text when you hover the Large image
 
     .setAssetsSmallText('ok') //Text when you hover the Small image
